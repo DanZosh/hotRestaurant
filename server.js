0758a22a8597e14b71b49d0722dc7567caf630f1
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2020-12-15 00:54:29 -0800

    added get request to view tables page made data appear with javasript

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ var express = require("express");
 const path = require("path");
 // const dataTablesArr = require("./dataTables");
 // const dataReservationsArr = require("./dataReservations");
-    // console.log(dataReservationsArr)
+// console.log(dataReservationsArr)
 
 // Sets up the Express App
 // ============================
 var app = express();
-var PORT = 3333;
+var PORT = process.env.PORT || 3333;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -18,61 +18,61 @@ app.use(express.json());
 
 
 //DATA
-const dataTablesArr=[
-    {name:'pup',
-    phone:'lump',
-    email:'sup',
-    id:'cup'},
-    
-    {name:'test',
-    phone:'crest',
-    email:'best',
-    id:'nest'},
-    
-    {name:'cat',
-    phone:'mat',
-    email:'blat',
-    id:'splat'},
-    
-    {name:'car',
-    phone:'mar',
-    email:'blar',
-    id:'splar'},
-    
-    {name:'yoda',
-    phone:'boda',
-    email:'noda',
-    id:'foda'}
+const dataTablesArr = [
+    // {name:'pup',
+    // phone:'lump',
+    // email:'sup',
+    // id:'cup'},
+
+    // {name:'test',
+    // phone:'crest',
+    // email:'best',
+    // id:'nest'},
+
+    // {name:'cat',
+    // phone:'mat',
+    // email:'blat',
+    // id:'splat'},
+
+    // {name:'car',
+    // phone:'mar',
+    // email:'blar',
+    // id:'splar'},
+
+    // {name:'yoda',
+    // phone:'boda',
+    // email:'noda',
+    // id:'foda'}
 ];
-const dataWaitListArr=[];
+const dataWaitListArr = [];
 
 // //PATHS 
 
 //PATH htmls
-app.get("/reservation", function(req, res) {
+app.get("/reservation", function (req, res) {
     res.sendFile(path.join(__dirname, "./reservation.html"));
 });
 
-app.get("/tables", function(req, res) {
+app.get("/tables", function (req, res) {
     res.sendFile(path.join(__dirname, "./tables.html"));
 });
 
 
 
 // Displays all tables
-app.get("/api/tables", function(req, res) {
+app.get("/api/tables", function (req, res) {
     console.log(dataTablesArr)
     return res.json(dataTablesArr);
-  });
+});
 
 // Displays all reservations
-app.get("/api/reservation", function(req, res) {
+app.get("/api/waitList", function (req, res) {
     console.log(dataWaitListArr)
     return res.json(dataWaitListArr);
-  });
+});
 
-  // Create New tables - takes in JSON input
-app.post("/api/tables", function(req, res) {
+// Create New tables - takes in JSON input
+app.post("/api/tables", function (req, res) {
 
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
@@ -82,12 +82,12 @@ app.post("/api/tables", function(req, res) {
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
     // newTable.uniquID = newCharacter.name.replace(/\s+/g, "").toLowerCase();
 
-    if (dataTablesArr.length < 5){
+    if (dataTablesArr.length < 5) {
         dataTablesArr.push(newTable);
         res.json(newTable);
         return true;
     }
-    else{
+    else {
         dataWaitListArr.push(newTable);
         res.json(newTable);
         return false;
@@ -96,10 +96,10 @@ app.post("/api/tables", function(req, res) {
 });
 
 
-app.use("/", function(req, res) {
+app.use("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./home.html"));
 });
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
 
