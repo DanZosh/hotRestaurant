9b4e287b3198bb7462914051cae52bd074ec7d93
Danny Maier <dannymaier@gmail.com>
2020-12-14 19:00:45 -0800

    successfully pathed the starter html pages

--- server.js
+++ server.js
@@ -1,11 +1,48 @@
-var express = require("express");
 
+// Dependencies
+// ============================
+var express = require("express");
+const path = require("path");
+
+// Sets up the Express App
+// ============================
 var app = express();
 var PORT = 3333;
 
-app.get("/", function(req, res) {
-    res.send("hello world!");
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// app.get("/", function(req, res) {
+//     res.send("hello world!");
+// });
+
+
+// //PATHS 
+
+//PATH htmls
+//PATH to home.html 
+app.get("/reservation", function(req, res) {
+    res.sendFile(path.join(__dirname, "./reservation.html"));
 });
+
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "./tables.html"));
+});
+
+app.use("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./home.html"));
+});
+
+// PATH to notes.html 
+const notes = app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./app/public/notes.html"));
+});
+
+
+
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
